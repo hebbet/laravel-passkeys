9f42fdac3fa5935ad996aee96c64b06b4db02ba9
Samuel De Backer <sdebacker@gmail.com>
2025-05-12 09:43:04 +0200

    startAuthentication refactor to expected call structure

--- resources/views/livewire/partials/createScript.blade.php
+++ resources/views/livewire/partials/createScript.blade.php
@@ -3,7 +3,7 @@
     Livewire.on('passkeyPropertiesValidated', async function (eventData) {
         const passkeyOptions = eventData[0].passkeyOptions;
 
-        const passkey = await startRegistration(passkeyOptions);
+        const passkey = await startRegistration({ optionsJSON: passkeyOptions });
 
         @this.call('storePasskey', JSON.stringify(passkey));
     });
