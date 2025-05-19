33f49b69cf181944a5cdedfcd8e47123b0a42906
Pieter Naber <github.com@mantix.nl>
2025-05-19 10:31:16 +0200

    Add French and Dutch translations for passkeys

---
+++ resources/lang/fr/passkeys.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'authenticate_using_passkey' => 'S\'authentifier avec la passkey',
+    'create' => 'Créer',
+    'delete' => 'Supprimer',
+    'error_something_went_wrong_generating_the_passkey' => 'Une erreur s\'est produite lors de la génération de la passkey.',
+    'invalid' => 'Impossible de se connecter avec la passkey fournie',
+    'last_used' => 'Dernière utilisation',
+    'name' => 'Nom',
+    'name_placeholder' => 'Entrez le nom de la passkey',
+    'no_passkeys_registered' => 'Aucune passkey enregistrée',
+    'not_used_yet' => 'Pas encore utilisée',
+    'passkeys' => 'Passkeys',
+];
