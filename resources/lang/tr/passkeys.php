fb1aeaf0a2dff93ed73f61a91591d30f92ae343f
Evrim Altay Koluaçık <evrimaltay@gmail.com>
2025-06-09 18:51:06 +0300

    Turkish translation of passkeys.php
    Translation of passkeys.php file to add Turkish localization support. Thanks for this great package!

---
+++ resources/lang/tr/passkeys.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'authenticate_using_passkey' => 'Geçiş Anahtarı Kullanarak Oturum Aç',
+    'create' => 'Oluştur',
+    'delete' => 'Sil',
+    'error_something_went_wrong_generating_the_passkey' => 'Geçiş anahtarı oluşturulurken bir şeyler hatalı gitti.',
+    'invalid' => 'Belirtilen geçiş anahtarı ile giriş yapılamadı.',
+    'last_used' => 'Son Kullanılma',
+    'name' => 'İsim',
+    'name_placeholder' => 'Geçiş Anahtarına Bir İsim Verin',
+    'no_passkeys_registered' => 'Kayıtlı bir geçiş anahtarı yok',
+    'not_used_yet' => 'Hiç Kullanılmadı',
+    'passkeys' => 'Geçiş Anahtarları',
+];
