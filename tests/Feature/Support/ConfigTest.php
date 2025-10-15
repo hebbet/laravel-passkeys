08ab4fe724372b4df4ceb716533e22943fb4fc97
Julius Egelkraut <j.egelkraut@liotec.de>
2025-10-15 11:26:24 +0200

    Test for new ConfigureCeremonyStepManagerFactoryAction added

--- tests/Feature/Support/ConfigTest.php
+++ tests/Feature/Support/ConfigTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Spatie\LaravelPasskeys\Actions\ConfigureCeremonyStepManagerFactoryAction;
 use Spatie\LaravelPasskeys\Actions\FindPasskeyToAuthenticateAction;
 use Spatie\LaravelPasskeys\Actions\GeneratePasskeyAuthenticationOptionsAction;
 use Spatie\LaravelPasskeys\Actions\GeneratePasskeyRegisterOptionsAction;
@@ -28,4 +29,6 @@ it('can get the default action classes', function () {
     expect(Config::getActionClass('generate_passkey_authentication_options', GeneratePasskeyAuthenticationOptionsAction::class))->not->toBeNull()->toBe(GeneratePasskeyAuthenticationOptionsAction::class);
 
     expect(Config::getActionClass('find_passkey', FindPasskeyToAuthenticateAction::class))->not->toBeNull()->toBe(FindPasskeyToAuthenticateAction::class);
+
+    expect(Config::getActionClass('configure_ceremony_step_manager_factory', ConfigureCeremonyStepManagerFactoryAction::class))->not->toBeNull()->toBe(ConfigureCeremonyStepManagerFactoryAction::class);
 });
